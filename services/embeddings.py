226fb62693300d8de665f53786749f8607d46a83
Carlos Jimenez <solracjh15@gmail.com>
2024-01-06 16:23:53 -0500

    use langchain to generate openai embeddings

--- services/embeddings.py
+++ services/embeddings.py
@@ -1,32 +1,17 @@
 import os
 from typing import List
 
-# from openai import AzureOpenAI
-from openai import OpenAI
+from langchain_community.embeddings.openai import OpenAIEmbeddings
 
 
 class Embedder:
     def __init__(self):
-        # todo check if I will need these env vars or if I need to change
-        #  them since I'm not using Azure OpenAI
-        self._api_key = os.environ.get("AZURE_OPENAI_API_KEY")
-        self._endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
+        self._api_key = os.environ.get("OPENAI_API_KEY")
         self._model = os.environ.get("OPENAI_MODEL")
-        # self._client = AzureOpenAI(
-        #     api_key=self._api_key,
-        #     api_version="2023-05-15",
-        #     azure_endpoint=self._endpoint,
-        # )
-        # todo finish adding parameters to client
-        self._client = OpenAI()
+        self._client = OpenAIEmbeddings(
+            openai_api_key=self._api_key, deployment=self._model, model=self._model
+        )
 
     def embed(self, text: str) -> List[float]:
         text = text.replace("\n", " ")
-        return (
-            self._client.embeddings.create(
-                input=[text],
-                model=self._model,
-            )
-            .data[0]
-            .embedding
-        )
+        return self._client.embed_query(text)
